4013072fdd04caeb872690cd8154f0b39b06b26a
Bogdan Smeyko <bogdan.smeyko@gmail.com>
2021-08-10 18:19:50 +0300

    Implement search filter validation

---
+++ API/Helpers/GetMembersResult.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Helpers
+{
+    public class GetMembersResult
+    {
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ API/Interfaces/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using API.DTOs;
+using API.Helpers;
+
+namespace API.Interfaces
+{
+    public interface IUserService
+    {
+        GetMembersResult GetMembers(UserParams userParams);
+    }
+}
